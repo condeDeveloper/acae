2101ddbba71b47ea2701e1a225b741edbaa583d1
condeDeveloper <dcavalcantevxi@gmail.com>
2026-03-13 07:17:21 -0300

    testeII

--- backend/prisma/seed-atividades.ts
+++ backend/prisma/seed-atividades.ts
@@ -537,7 +537,7 @@ async function main() {
 
   let inseridas = 0
   let ignoradas = 0
-  let teste = 0;
+  let teste = 1;
 
   for (const atividade of atividades) {
     const existente = await prisma.atividadeBNCC.findFirst({
